8cdc9ad1484d8cfd4e99e275a93bb036e8782551
prateekmtri <prateek1tri2@gmail.com>
2026-02-14 17:23:29 +0530

    added url

--- frontend/app/authentication/Login/page.js
+++ frontend/app/authentication/Login/page.js
@@ -23,7 +23,7 @@ export default function LoginPage() {
     
     try {
       // Backend API Call
-      const response = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+      const response = await fetch("https://resume-checker-zhh3.onrender.com/api/v1/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
